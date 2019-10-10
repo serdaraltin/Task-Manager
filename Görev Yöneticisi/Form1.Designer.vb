c6fad763536cfeff770f9b68953d35ed7455edf0
Serdar Eyüp ALTIN <43209941+serdareyupaltin@users.noreply.github.com>
2019-10-11 01:27:44 +0300

    Add files via upload

---
+++ Görev Yöneticisi/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6})
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(0, 0)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(841, 316)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "PID"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "AD"
+        Me.ColumnHeader2.Width = 181
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "RAM"
+        Me.ColumnHeader3.Width = 95
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Dizin"
+        Me.ColumnHeader4.Width = 252
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Thread"
+        Me.ColumnHeader5.Width = 59
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Width = 99
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(841, 316)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ Görev Yöneticisi/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Diagnostics
+Public Class Form1
+  
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ListView1.Items.Clear()
+        For Each ps As Process In Process.GetProcesses(My.Computer.Name)
+            Dim items As ListViewItem = New ListViewItem(ps.Id.ToString())
+            items.SubItems.Add(ps.ProcessName)
+            items.SubItems.Add(FormatNumber(Math.Round(ps.PagedMemorySize64 / 1024), 0).ToString())
+            Try
+                items.SubItems.Add(ps.MainModule.FileName)
+            Catch
+                items.SubItems.Add("n/a")
+            End Try
+            items.SubItems.Add(ps.Threads.Count)
+
+            ListView1.Items.Add(items)
+        Next
+    End Sub
+
+End Class
